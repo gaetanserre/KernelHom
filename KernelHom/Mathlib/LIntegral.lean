cb89dc6d653dc4d3f600ce782de16c0b83ffacdd
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-05-29 17:47:14 +0200

    modules

--- KernelHom/Mathlib/LIntegral.lean
+++ KernelHom/Mathlib/LIntegral.lean
@@ -3,7 +3,6 @@ Copyright (c) 2026 Gaëtan Serré. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gaëtan Serré
 -/
-
 module
 
 public import Mathlib.MeasureTheory.Integral.Lebesgue.Countable
