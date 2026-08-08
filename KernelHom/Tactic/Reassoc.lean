93b38907af218c29b101949212ebbe52e1d01ad9
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2026-08-08 04:02:48 +0200

    WIP reassoc

---
+++ KernelHom/Tactic/Reassoc.lean
@@ -0,0 +1,57 @@
+/-
+Copyright (c) 2026 Gaëtan Serré. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Gaëtan Serré
+-/
+module
+
+public import Mathlib.Tactic.CategoryTheory.Reassoc
+public import KernelHom.Tactic.KernelHom
+
+/-!
+-/
+
+public meta section
+
+open Lean Meta Elab Tactic
+
+namespace Mathlib.Tactic.Reassoc
+
+def dummyGoal : MetaM MVarId := do
+  return (← mkFreshExprMVar (mkConst ``True)).mvarId!
+
+def runTacticMInMetaM (goal : MVarId) (tac : TacticM Expr) : MetaM Expr := do
+  let (some e, _) ← (Tactic.run_for goal tac).run'
+    | throwError "la tactique n'a pas produit de résultat"
+  instantiateMVars e
+
+def runTacticMInMetaM' (goal : MVarId) (tac : TacticM α) : MetaM α := do
+  let ctx : Tactic.Context := { elaborator := .anonymous, recover := false }
+  goal.withContext do
+    let (a, _) ← ((tac.run ctx).run { goals := [goal] }).run' {} {}
+    pure a
+
+def kernelReassocHandler (e : Expr) : MetaM (Expr × Array MVarId) := do
+  logInfo m!"kernelReassocHandler: {e}"
+  let mvar ← dummyGoal
+  let e_type ← inferType e
+  let some (_, lhs, rhs) := e_type.eq? | throwError "kernelReassocHandler: l'expression n'est pas une égalité"
+  logInfo m!"kernelReassocHandler: {lhs}"
+  let (X, Y, xLvl, yLvl) ← getTypesFromKernel lhs
+  logInfo m!"kernelReassocHandler: {X}, {Y}, {xLvl}, {yLvl}"
+  let (_, e_proof) ← HomEquality e_type
+  let e_proof ← mkAppM ``Eq.mp #[e_proof, e]
+  logInfo m!"kernelReassocHandler: {e_proof}"
+  let (ee, _) ← reassocExprHom e_proof
+  logInfo m!"kernelReassocHandler2: {ee}"
+  return (e, #[])
+
+initialize registerReassocExpr kernelReassocHandler
+
+open Lean Elab Tactic Meta in
+elab "test_handler " t:term : tactic => do
+  let e ← elabTerm t none
+  let (e', insts) ← Mathlib.Tactic.Reassoc.kernelReassocHandler e
+  logInfo m!"résultat: {e'}"
+
+end Mathlib.Tactic.Reassoc
---
+++ KernelHom/Tactic/Test.lean
@@ -0,0 +1,41 @@
+/-
+Copyright (c) 2026 Gaëtan Serré. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Gaëtan Serré
+-/
+import KernelHom.Tactic.Reassoc
+
+/-!
+-/
+
+open ProbabilityTheory CategoryTheory
+
+section
+
+variable {X Y : Type*} [MeasurableSpace X] [MeasurableSpace Y] (κ η : Kernel X Y) (h : κ = η)
+  [IsSFiniteKernel κ] [IsSFiniteKernel η]
+
+include h in
+@[reassoc]
+lemma test : κ = η := h
+
+#check test_assoc
+
+
+example (h : κ = η) : κ = η := by
+  test_handler h
+  exact h
+
+end
+
+section
+
+variable {C : Type*} [Category C] {X Y : C} (f g : X ⟶ Y) (h : f = g)
+
+include h in
+@[reassoc]
+lemma test2 : f = g := h
+
+#check test2_assoc
+
+end
